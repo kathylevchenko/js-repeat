b16dface9a518052a2b6f7f88ea233d215d4fd1a
Kateryna Davidovych <kathylevchenko1@gmail.com>
2024-06-30 22:00:42 +0300

    1

--- 1-file.js
+++ 1-file.js
@@ -1,33 +1,74 @@
-function makeTransaction(quantity,pricePerDroid){
-    const totalPrice = quantity * pricePerDroid;
-return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+// function makeTransaction(quantity,pricePerDroid){
+//     const totalPrice = quantity * pricePerDroid;
+// return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+// }
+
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+
+
+// function getShippingMessage(country,price,deliveryFee){
+//     const totalPrice = price + deliveryFee;
+// return `Shipping to ${country} will cost ${totalPrice} credits`
+// }
+// console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+// console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+// console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+
+
+function slugify(title){
+    const letters= title.toLowerCase()
+    const slug = letters.split(" ");
+    const result= slug.join("-")
+    return result;
+}
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+
+
+function makeArray(firstArray, secondArray, maxLength){
+let twoArr = firstArray.concat(secondArray);
+if(twoArr.length>maxLength){
+    return twoArr.slice(0,maxLength)
+} else{
+    return twoArr;
+}
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
 
 
-function getShippingMessage(country,price,deliveryFee){
-    const totalPrice = price + deliveryFee;
-return `Shipping to ${country} will cost ${totalPrice} credits`
+
+
+
+function filterArray(numbers, value){
+    let newArr = [];
+    for(let number of numbers){
+        if(number>value){
+           newArr.push(number)
+        }
+       
+    }
+    return newArr;
 }
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
 
 
-
-
-function getElementWidth(content,padding,border){
-const contentWidth = parseFloat(content);
-const paddingWidth = parseFloat(padding)
-const borderWidth = parseFloat(border)
-
-const totalResult = 
-}
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
